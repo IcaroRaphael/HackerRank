247420c617e2963dc174a26c898c7b2ff53c1b55
IcaroRaphael <icaro2551@gmail.com>
2023-06-20 16:36:22 -0300

    Java:ex006

---
+++ Java/project/src/ex006/Solution.java
@@ -0,0 +1,17 @@
+package ex006;
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        for(int i = 1; i <= 10; i++){
+            System.out.printf("%d x %d = %d\n", N, i, (N*i));
+        }
+
+        sc.close();
+    }
+}
